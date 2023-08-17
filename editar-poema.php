d5945cfd4636f34c50dd01bb77c64b97bfe89885
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-08-17 20:06:59 -0300

    agregar opcion editar poema

--- editar-poema.php
+++ editar-poema.php
@@ -4,13 +4,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="navbar.css">
-    <title>Document</title>
+    <?php
+    require 'links.php'
+    ?>
+    <link rel="stylesheet" href="editar-poema.css">
+    <title>Editar</title>
 </head>
 <body>
     <?php
     require 'navbar.php';
+    require 'mostrar-poemas.php';
     ?>
-    
+    <div class='editar'>
+    <form action="editar.php" method="post">
+    <label for="id_poema">Ingrese el ID a Editar: </label>
+    <br>
+    <input type="text" id="id_poema" name="id_poema" required>
+    <br>
+    <label for="nuevo-titulo">Ingrese el nuevo titulo: </label>
+    <input type="text" name="nuevo-titulo" id="titulo" required>
+    <br>
+    <label for="nuevo-poema">Nuevo poema: </label>
+    <textarea name="nuevo-poema" id="poema" cols="30" rows="10" required>
+    </textarea>
+    <input type="submit" name='editar' value="Editar">
+    </form>
+    </div>
+    <br><br>
 </body>
 </html>
---
+++ editar.php
@@ -0,0 +1,77 @@
+
+<?php
+require 'config.php';
+$error = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtener el ID del poema enviado desde el formulario
+    $id_poema = $_POST["id_poema"];
+    $nuevo_poema = $_POST["nuevo-poema"];
+    $nuevo_titulo = $_POST["nuevo-titulo"];
+    // Preparar la consulta SQL con una consulta preparada
+    //Utiliza un marcador de posición ? para el valor del ID.
+    $sql = "SELECT id_poema, poema, nombre FROM poemas WHERE id_poema = ?";
+    
+    // Preparar la consulta y vincular el parámetro
+    $stmt = $conexion->prepare($sql);
+    $stmt->bind_param("i", $id_poema);
+    
+    // Ejecutar la consulta
+    if ($stmt->execute()) {
+        $resultado = $stmt->get_result();
+        
+        if ($resultado->num_rows > 0) {
+            $error = "Se encontró el poema.";
+            $poemaEncontrado = true;
+            // Aquí puedes trabajar con los datos encontrados en $resultado
+        } else {
+            $error = "No se encontró ningún poema con ese ID.";
+        }
+    } else {
+        $error = "Error al ejecutar la consulta: " . $conexion->error;
+    }
+    //preparar el borrado del elemento
+    if ($poemaEncontrado && isset($_POST["editar"])) {
+    // Preparar la consulta SQL para eliminar el poema por su ID
+    $sql_update = "UPDATE poemas SET poema = ? , nombre = ? WHERE id_poema = ?";
+    
+    // Preparar la consulta y vincular el parámetro
+    $stmt_update = $conexion->prepare($sql_update);
+    $stmt_update->bind_param("ssi", $nuevo_poema, $nuevo_titulo, $id_poema);
+    
+    // Ejecutar la consulta de edicion
+    if ($stmt_update->execute()) {
+        $error = "El poema se edito exitosamente.";
+        // Restablecer el estado
+        $poemaEncontrado = false;
+    } else {
+        $error = "Error al Editar el poema: " . $conexion->error;
+    }
+    
+    // Cerrar la consulta de eliminación
+    $stmt_update->close();
+}
+
+
+    // Cerrar la consulta
+    $stmt->close();
+}
+
+// Cerrar la conexión
+$conexion->close();
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Editar</title>
+    </head>
+    <body>
+        <h1><?php
+        echo $error;
+        ?></h1>
+        <span style="text-align: center">Volver al form</span>
+        <a href="editar-poema.php">Click Aqui</a>
+    </body>
+    </html>
