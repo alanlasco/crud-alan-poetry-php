7d441723e6a996fae4a81e9ce7777500aca9c477
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-11-30 19:26:19 -0300

    agregar login y logout basicos

---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+
+session_destroy();                    //destruyo la sesion
+
+header('Location:login.php');         //retorno a la pagina principal
+
+
+ ?>
--- navbar.php
+++ navbar.php
@@ -5,6 +5,9 @@
         <li><a href="agregar-poema.php">Agregar</a></li>
         <li><a href="eliminar-poema.php">Eliminar</a></li>
         <li><a href="editar-poema.php">Editar</a></li>
+        <li><form action="logout.php" method="post">
+        <button type="submit">Cerrar Sesión</button>
+</form></li>
     </ul>
 </nav>
 
